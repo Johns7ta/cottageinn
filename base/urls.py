df2c4fff04d2654b12ab821da207724221aef5be
Johns7ta <johns7ta@cmich.edu>
2013-12-27 09:40:40 -0500

    Login & Create Account screens

--- base/urls.py
+++ base/urls.py
@@ -9,6 +9,8 @@ urlpatterns = patterns('',
 
     url(r'^orderbase/', 'orderapp.views.orderbase', name='orderbase'),
     url(r'^empbase/', 'empapp.views.empbase', name='empbase'),
+    url(r'^custlogin/', 'orderapp.views.custlogin', name='custlogin'),
+    url(r'^createacct/', 'orderapp.views.createacct', name='createacct'),
 
     url(r'^admin/', include(admin.site.urls)),
 
--- orderapp/views.py
+++ orderapp/views.py
@@ -9,6 +9,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.decorators.csrf import csrf_exempt
 from django.core.context_processors import csrf
 from django.core.urlresolvers import reverse
+from orderapp.forms import UserCreateForm
+from django.contrib.auth.models import User
 import simplejson
 
 
@@ -18,4 +20,61 @@ def orderbase(request,template_name="orderbase.html"):
 
     return render(template_name, context, context_instance=RequestContext(request))
 
+def custlogin(request, template_name="custlogin.html"):
+    context = {}
+    context.update(csrf(request))
+    next = None
 
+    try:
+        next = request.GET['next']
+
+    except:
+        next = ""
+
+
+    auth_frm = AuthenticationForm(data=request.POST or None)
+    context['form'] = auth_frm
+
+    if auth_frm.is_valid():
+        try:
+            user = authenticate(username=auth_frm.cleaned_data['username'], password=auth_frm.cleaned_data['password'])
+            login(request, user)
+        except Exception:
+            pass
+
+            #return the same form and say login failed
+
+        return HttpResponseRedirect('')
+
+    return render(template_name, context, context_instance=RequestContext(request))
+
+def createacct(request, template_name="createacct.html"):
+    context = {}
+    context.update(csrf(request))
+    next = None
+
+    try:
+        next = request.GET['next']
+
+    except:
+        next = "/custlogin/"
+
+
+    create_form = UserCreateForm(data=request.POST or None)
+    context['form'] = create_form
+
+    if request.method == 'POST':
+        form = create_form
+        if form.is_valid():
+            try:
+                new_user = form.save()
+                return HttpResponseRedirect('/custlogin/')
+            except:
+                return HttpResponse("Cannot save form.")
+        else:
+            form = create_form
+        return render(request, "createacct.html", {
+            'form': form,
+        })
+
+    return render(template_name, context, context_instance=RequestContext(request))
